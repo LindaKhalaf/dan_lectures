f88c4b3b6136d40fabe69ca11d750e7fe2acd545
Linda Khalaf <khalaflb24@gmail.com>
2015-01-15 10:48:48 -0800

    Fixed the errors in my last push

--- day2_construct_lecture_/index.php
+++ day2_construct_lecture_/index.php
@@ -1,45 +1,4 @@
 <?php  
-//Lecture 1
-//______________________________________________________________________________________________________
-//example 1
-	class Sup {
-		public $firstName = "nien";
-		function getName() {
-			return "{$this->firstName}";
-		}
-	}
-	$sup1 = new sup();
-	$sup1->firstName = "Sup";
-	print "(1){$sup1->getName()}.";
-
-	//example 2
-	class Food {
-		public $apple_f00d = "red";
-		public $pencil = "nien";
-		public $price = 2.99;
-		function getf00d() {
-			return "{$this->apple_f00d}";
-		}
-		function getf00ds() {
-			return "{$this->price}";
-		}
-	}
-	$food1 = new food();
-	$food1->apple = "is red";
-	print " (2) An apple is {$food1->getf00d()} and costs " . "{$food1->getf00ds()}";
-
-//example 2
-	class cat {
-		public $firstName = "pico";
-		public $gender = "boy";
-		public $price = 0;
-		function getName() {
-			return "{$this->firstName}";
-		}
-	}
-	$cat1 = new cat();
-	$cat1->firstName = "pico";
-	print " (3) My cat's name is {$cat1->getName()}.";
 //_________________________________________________________________________________________________________
 //lecture 2
 //example 1
@@ -58,22 +17,22 @@
 	}
 //EXAMPLE 2
 	class Food {
-		public $apple_f00d;
-		public $pencil;
-		public $price;
+		public $apple_f00d;
+		public $pencil;
+		public $price;
 		function __construct($apple_f00d, $pencil, $price) {
-			$this->apple_f00d = $apple_f00d;
-			$this->pencil = $pencil;
-			$this->price = $price;
+			$this->apple_f00d = $apple_f00d;
+			$this->pencil = $pencil;
+			$this->price = $price;
 		}
 		function getName() {
 			return "{$this->apple_f00d}" .
-			"{$this->pencil}";
+			"{$this->pencil}";
 		}
 	}
 //example 3
 	class Sup {
-		public $firstName;
+		public $firstName;
 		function __construct($firstName) {
 			$this->firstName = $firstName;
 		}
@@ -81,4 +40,8 @@
 			return "{$this->firstName}";
 		}
 	}
+
+	 $it = new Sup('Linda');
+
+  echo $it->getName();
 ?>
